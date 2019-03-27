af880524fa2e7bea18d60a58e9016c4dfd5eebcb
Galiful <1907317610@qq.com>
2019-03-27 15:03:13 +0800

    新增App截图

--- app/src/main/java/com/example/galier/ble/MainActivity.java
+++ app/src/main/java/com/example/galier/ble/MainActivity.java
@@ -115,7 +115,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-
         ibup.setOnTouchListener(this);
         ibdown.setOnTouchListener(this);
         ibleft.setOnTouchListener(this);
